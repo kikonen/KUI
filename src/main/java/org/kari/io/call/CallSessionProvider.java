35d649685723516ff89670760795c2ae12d24a30
kari <mr.kari.ikonen@gmail.com>
2013-02-17 14:59:23 +0200

    CALL: Improved API spec

--- src/main/java/org/kari/io/call/CallSessionProvider.java
+++ src/main/java/org/kari/io/call/CallSessionProvider.java
@@ -7,5 +7,13 @@ package org.kari.io.call;
  * @author kari
  */
 public interface CallSessionProvider {
+    /**
+     * <p>NOTE KI It's important to return *SAME* sessionId instance always
+     * unless it changes. Based into this strict identity equality framework
+     * is doing optimization communication to avoid passing sessionId redundantly
+     * over network.
+     * 
+     * @return sessionId, can be null if no session used
+     */
     Object getSessionId();
 }

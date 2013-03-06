561303942ae3bf7c0cfe9d3ec1e1b8fd8756ad45
Kari Ikonen <mr.kari.ikonen@gmail.com>
2013-03-06 22:14:33 +0200

    CALL/IO: Performance obsession; removed synchronized methods - FIX: Ensure that byte IO buffers used by call API won't trigger   synchronized

--- call/src/main/java/org/kari/call/io/DirectByteArrayInputStream.java
+++ call/src/main/java/org/kari/call/io/DirectByteArrayInputStream.java
@@ -4,7 +4,9 @@ import java.io.ByteArrayInputStream;
 
 /**
  * Byte stream, which allows resetting buffer
- *
+ * 
+ * <p>NOTE: KI Several methods overridden to avoid synchronized
+ * 
  * @author kari
  */
 public final class DirectByteArrayInputStream extends ByteArrayInputStream {
@@ -22,6 +24,10 @@ public final class DirectByteArrayInputStream extends ByteArrayInputStream {
         super(EMPTY_BUFFER, 0, 0);
     }
 
+    /**
+     * Allow resetting internal buffer into another one. Useful in case this 
+     * input stream is source some other reused input stream.
+     */
     public void set(byte[] pBuf, int pOffset, int pLength) {
         buf = pBuf;
         pos = pOffset;
@@ -29,11 +35,83 @@ public final class DirectByteArrayInputStream extends ByteArrayInputStream {
         mark = pOffset;
     }
     
+    /**
+     * Changes buffer into "byte[0]" buffer
+     */
     public void empty() {
         set(EMPTY_BUFFER, 0, 0);
     }
-    
+
+    /**
+     * @return Direct reference to buffer
+     */
     public byte[] getBuffer() {
         return buf;
     }
+    
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public int read() {
+        return (pos < count) ? (buf[pos++] & 0xff) : -1;
+    }
+
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public int read(byte b[], int off, int len) {
+        if (b == null) {
+            throw new NullPointerException();
+        } else if (off < 0 || len < 0 || len > b.length - off) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (pos >= count) {
+            return -1;
+        }
+
+        int avail = count - pos;
+        if (len > avail) {
+            len = avail;
+        }
+        if (len <= 0) {
+            return 0;
+        }
+        System.arraycopy(buf, pos, b, off, len);
+        pos += len;
+        return len;
+    }
+
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public long skip(long n) {
+        long k = count - pos;
+        if (n < k) {
+            k = n < 0 ? 0 : n;
+        }
+
+        pos += k;
+        return k;
+    }
+
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public int available() {
+        return count - pos;
+    }
+
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public void reset() {
+        pos = mark;
+    }
+
 }
--- call/src/main/java/org/kari/call/io/DirectByteArrayOutputStream.java
+++ call/src/main/java/org/kari/call/io/DirectByteArrayOutputStream.java
@@ -1,9 +1,12 @@
 package org.kari.call.io;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 
 /**
  * Direct byte array output stream
+ * 
+ * <p>NOTE: KI Several methods overridden to avoid synchronized
  * 
  * @author kari
  */
@@ -28,4 +31,68 @@ public final class DirectByteArrayOutputStream
     public byte[] getBuffer() {
         return buf;
     }
+    
+    private void ensureCapacity(int minCapacity) {
+        // overflow-conscious code
+        if (minCapacity - buf.length > 0)
+            grow(minCapacity);
+    }
+
+    /**
+     * <pTODO KI Use BufferPool for buffer allocation
+     */
+    private void grow(int minCapacity) {
+        // overflow-conscious code
+        int oldCapacity = buf.length;
+        int newCapacity = oldCapacity << 1;
+        if (newCapacity - minCapacity < 0)
+            newCapacity = minCapacity;
+        if (newCapacity < 0) {
+            if (minCapacity < 0) // overflow
+                throw new OutOfMemoryError();
+            newCapacity = Integer.MAX_VALUE;
+        }
+        buf = Arrays.copyOf(buf, newCapacity);
+    }
+
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public void write(int b) {
+        ensureCapacity(count + 1);
+        buf[count] = (byte) b;
+        count += 1;
+    }
+
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public void write(byte b[], int off, int len) {
+        if ((off < 0) || (off > b.length) || (len < 0) ||
+            ((off + len) - b.length > 0)) {
+            throw new IndexOutOfBoundsException();
+        }
+        ensureCapacity(count + len);
+        System.arraycopy(b, off, buf, count, len);
+        count += len;
+    }
+
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public void reset() {
+        count = 0;
+    }
+
+    /**
+     * <p>NOTE: KI overridden to avoid synchronized
+     */
+    @Override
+	public int size() {
+        return count;
+    }
+
 }
